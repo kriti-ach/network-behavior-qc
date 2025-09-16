df3202217031011b311da748d44611107ee7e950
kriti-a567 <kriti.a.64@gmail.com>
2025-09-16 07:12:37 -0700

    removed subject_id_numeric column from aggregated violations

--- src/network-out-of-scanner-qc/utils/utils.py
+++ src/network-out-of-scanner-qc/utils/utils.py
@@ -383,8 +383,8 @@ def filter_to_test_trials(df, task_name):
 
 def sort_subject_ids(df):
     df['subject_id_numeric'] = df['subject_id'].str.replace('s', '').astype(int)
-    df = df.sort_values(by='subject_id_numeric', ascending=True)
-    df = df.drop(columns=['subject_id_numeric'])
+    df.sort_values(by='subject_id_numeric', ascending=True, inplace=True)
+    df.drop(columns=['subject_id_numeric'], inplace=True)
     return df
 
 def update_qc_csv(output_path, task_name, subject_id, metrics):
